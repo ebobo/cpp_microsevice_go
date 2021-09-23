725c15042dbc33d8a7f8bd657d0b337bc813763c
qi xu <qi.xu@autronicafire.no>
2021-09-23 18:48:31 +0200

    added command line parser

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+)
+
+// Options contains the command line options
+type Options struct {
+	Message  string `short:"m" long:"message" env:"MESSAGE" description:"The input message" required:"yes"`
+}
 
 func main() {
-	log.Println("Hello")
+	var opt Options
+	parser := flags.NewParser(&opt, flags.Default)
+	_, err := parser.Parse()
+	if err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+	fmt.Printf("message %s\n", opt.Message)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,9 @@ module github.com/ebobo/cpp_microservice_go
 
 go 1.17
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/jessevdk/go-flags v1.5.0
+)
+
+require golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
