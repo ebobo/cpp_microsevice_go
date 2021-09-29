559571c820bba5c64d94c1298b90db5ab09066cb
qi xu <qi.xu@autronicafire.no>
2021-09-29 09:39:12 +0200

    client ms wip

--- backend/calculator/calc.service.go
+++ backend/calculator/calc.service.go
@@ -8,10 +8,14 @@ type CalcMicroService struct {
 	grpc_client *GrpcClient
 }
 
+func NewCalcMicroService() *CalcMicroService {
+	return &CalcMicroService{}
+}
+
 // Run runs whole algorithm to process maps
 func (cms *CalcMicroService) Run() {
 	log.Println("Running calculation micro service")
-	cms.grpc_client = NewGrpcClient("localhost:7601")
+	cms.grpc_client = NewGrpcClient("localhost:5006")
 
 	err := cms.grpc_client.Init()
 	if err != nil {
